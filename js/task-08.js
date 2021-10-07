89e0df17461229d5b5609326042861229b152bf0
zTjamm <jamm85@bk.ru>
2021-10-07 13:37:54 +0300

    9
    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector('.login-form');
+const allValue = {};
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+    if (email.value == "" || password.value == "") {
+        alert('Все поля должны быть заполнены.');
+    } else {        
+        allValue.email = email.value;
+        allValue.password = password.value;
+        console.log(allValue)
+        loginForm.reset();
+    }    
+})
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+button.addEventListener('click', () => {
+  body.style.background = getRandomHexColor();
+  textColor.innerHTML = getRandomHexColor();
+})
